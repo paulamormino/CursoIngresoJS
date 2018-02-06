5596fbcccec0697358fe81b4d53a3102b9a84388
Paula Mormino <36174321+paulamormino@users.noreply.github.com>
2018-02-05 21:02:46 -0300

    Ejercicios 1 y 2
    prompt variables concatenar

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,19 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	// alert(); //
+	var nombre;
+/*
+	alert("nombre");
+	alert(nombre);
+	nombre="Paula";
+	alert(nombre);
+
+
+	//nombre=prompt("Ingrese su nombre" , "natalia natalia");
+	alert(nombre);
+	*/
+alert("su nombre")  +nombre;
+//var person = prompt("Please enter your name", "Paula Mormino");
 }
 
